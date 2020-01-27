5595ce015cd8efafdd454b61906c8e1d645c2e74
임근석 <rmstjr1030@nate.com>
2020-01-27 08:39:18 +0000

    Update code

--- server.py
+++ server.py
@@ -24,17 +24,29 @@ def _apply():
 def _search():
     conn = lite.connect(database_filename)
     cs = conn.cursor()
-    query = "SELECT COUNT(*) FROM redirect WHERE redirecturl='%s';" % (request.get_json()['url'])
+    query = "SELECT COUNT(*) FROM redirect WHERE redirecturl='%s' and tag='Default';" % (request.get_json()['url'])
     cs.execute(query)
     count = cs.fetchall()
     if count[0][0] == 0:
         cs.execute("SELECT COUNT(*) FROM redirect;")
         _count = cs.fetchall()
         compat_url = URL_Shortener().shorten_url(request.get_json()['url'], _count[0][0] + 1)
-        data = {"Code": "200", "url": compat_url}
+        cs.close()
+        conn.close()
+        data = {"Code": "200", "url": compat_url, "size": 1, "tag": {"1":"Default"}}
         return data
     elif count[0][0] > 0:
-        data = {"Code": "409"}
+        cs.execute("SELECT tag FROM redirect WHERE redirecturl='%s';" % (request.get_json()['url']))
+        _tag = cs.fetchall()
+        cs.close()
+        conn.close()
+        data = {"Code": "409", "size": len(_tag), "tag": None}
+        list = {}
+        stack = 1
+        for i in _tag[0]:
+            list[stack] = i
+            stack = stack + 1
+        data["tag"] = list
         return data
 
 @app.route('/apply', methods=['POST'])
@@ -44,14 +56,25 @@ def _applyurl():
     cs.execute("SELECT COUNT(*) FROM redirect;")
     count = cs.fetchall()
     compat_url = URL_Shortener().shorten_url(request.get_json()['redirecturl'], count[0][0] + 1)
-    cs.execute("INSERT INTO redirect (redirecturl, shareurl) values ('%s', '%s')" % (request.get_json()['redirecturl'], compat_url[28:]))
-    # cs.execute("INSERT INTO redirect (redirecturl, shareurl) values ('%s', '%s')" % (request.get_json()['redirecturl'], compat_url[23:]))
+    cs.execute("INSERT INTO redirect (redirecturl, shareurl, tag) values ('%s', '%s', '%s')" % (request.get_json()['redirecturl'], compat_url[28:], request.get_json()['tag']))
+    # cs.execute("INSERT INTO redirect (redirecturl, shareurl, tag) values ('%s', '%s', '%s')" % (request.get_json()['redirecturl'], compat_url[23:], request.get_json()['tag']))
     conn.commit()
     cs.close()
     conn.close()
     data = {"Code": "201"}
     return data
 
+@app.route('/tag/add', methods=['POST'])
+def _addtag():
+    conn = lite.connect(database_filename)
+    cs = conn.cursor()
+    cs.execute("SELECT COUNT(*) FROM redirect;")
+    count = cs.fetchall()
+    compat_url = URL_Shortener().shorten_url(request.get_json()['redirecturl'], count[0][0] + 1)
+    cs.close()
+    conn.close()
+    data = {"Code": "201", "url": compat_url, "tag": request.get_json()['tag']}
+    return data
 '''
 type = 유입 플랫폼
 '''
@@ -84,8 +107,8 @@ def _get(type):
 
 if __name__ == "__main__":
     cs = conn.cursor()
-    query = "CREATE TABLE IF NOT EXISTS hitlog (id INTEGER PRIMARY KEY AUTOINCREMENT, k_redirect INTEGER, tag VARCHAR(64) DEFAULT 'default', url VARCHAR(256), time DATETIME, FOREIGN KEY(k_redirect) REFERENCES redirect(id))"
+    query = "CREATE TABLE IF NOT EXISTS redirect (id INTEGER PRIMARY KEY AUTOINCREMENT, redirecturl VARCHAR(256), shareurl VARCHAR(256), tag VARCHAR(64) DEFAULT 'Default')"
     cs.execute(query)
-    query = "CREATE TABLE IF NOT EXISTS redirect (id INTEGER PRIMARY KEY AUTOINCREMENT, redirecturl VARCHAR(256), shareurl VARCHAR(256))"
+    query = "CREATE TABLE IF NOT EXISTS hitlog (id INTEGER PRIMARY KEY AUTOINCREMENT, k_redirect INTEGER, url VARCHAR(256), time DATETIME, FOREIGN KEY(k_redirect) REFERENCES redirect(id))"
     cs.execute(query)
     app.run(host='0.0.0.0', port=80)
